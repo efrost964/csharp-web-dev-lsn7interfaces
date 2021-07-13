223285e15f48f2692d73bafc02d2b5921ea2e8f0
The_Lump <thelump@lump.com>
2021-07-13 16:07:42 -0400

    finished excercises

--- IceCreamShop/Program.cs
+++ IceCreamShop/Program.cs
@@ -10,14 +10,38 @@ namespace IceCreamShop
             Case menu = new Case();
             List<Flavor> availableFlavors = menu.Flavors;
             List<Cone> availableCones = menu.Cones;
+            FlavorComparer comparer = new FlavorComparer();
+            ConeComparer cComparer = new ConeComparer();
+            foreach (Flavor flavor in availableFlavors)
+            {
+                Console.WriteLine(flavor.Name);
+            }
+            availableFlavors.Sort(comparer);
+            Console.WriteLine("<------------------------>");
+            foreach (Flavor flavor in availableFlavors)
+            {
+                Console.WriteLine(flavor.Name);
+            }
 
-        // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
-        //  field.
 
-        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
-        //  field.
+            Console.WriteLine("<-----------CONES------------->");
+            foreach(Cone cone in availableCones)
+            {
+                Console.WriteLine($"{cone.Name} {cone.Cost}");
+            }
+            availableCones.Sort(cComparer);
+            Console.WriteLine("<------------------------>");
+            foreach (Cone cone in availableCones)
+            {
+                Console.WriteLine($"{cone.Name} {cone.Cost}");
+            }
+            // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
+            //  field.
 
-        // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+            // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
+            //  field.
+
+            // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
         }
     }
 }
